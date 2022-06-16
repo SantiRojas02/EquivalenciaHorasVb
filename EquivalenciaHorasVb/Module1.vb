cf52e4b22e2327c458666f6efe288c935491a759
SantiRojas <sr2897040@gmail.com>
2022-06-16 14:09:39 -0300

    EquivalenciaHorasVb

---
+++ EquivalenciaHorasVb/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim cantidadHoras As Integer
+        Dim minutos As Decimal
+        Dim segundos As Decimal
+        Dim dias As Integer
+
+        Console.Write("INGRESE CANTIDAD DE HORAS: ")
+        cantidadHoras = Console.ReadLine()
+
+        minutos = cantidadHoras * 60
+        Console.WriteLine("Minutos: " & minutos)
+        segundos = minutos * 60
+        Console.WriteLine("Segundos: " & segundos)
+        dias = cantidadHoras * 0.041666
+        Console.WriteLine("Dias: " & dias)
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
